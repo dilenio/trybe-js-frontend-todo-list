c743dc7b8383baf3978c65f850e7138fadd6f55a
Dilenio Enderle <dilenio@gmail.com>
2020-07-29 18:23:57 -0300

    12 - Adicione um botão com id=salvar-tarefas que salve o conteúdo da lista. Se você fechar e reabrir a página, a lista deve continuar no estado em que estava.

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+// Salva no LocalStorage as tarefas
+function saveToLocalStorage() {
+  localStorage.clear();
+  const tasks = document.querySelector('#lista-tarefas');
+  const objTasks = [];
+  const objClasses = [];
+  for (let index = 0; index < tasks.children.length; index += 1) {
+    objTasks[index] = tasks.children[index].innerText;
+    objClasses[index] = tasks.children[index].classList;
+  }
+  localStorage.setItem('tasks', objTasks.join());
+  localStorage.setItem('classes', objClasses.join());
+}
+
+// Recupera as tarefas do LocalStorage
+function getFromLocalStorage() {
+  const objTasks = localStorage.getItem('tasks').split(',');
+  const objClasses = localStorage.getItem('classes').split(',');
+  for (let index = 0; index < objTasks.length; index += 1) {
+    const addTask = document.querySelector('#lista-tarefas');
+    const list = document.createElement('li');
+    list.innerHTML = objTasks[index];
+    list.className = objClasses[index];
+    list.id = 'lista';
+    addTask.appendChild(list);
+    list.addEventListener('click', handleClickTaskElement);
+    list.addEventListener('dblclick', handleDblclickTaskElement);
+  }
+}
+
 // Lidar com o evento click na tarefa
 // Remove a seleção de itens e adiciona apenas naquele que foi clicado.
 function handleClickTaskElement(event) {
@@ -24,12 +54,14 @@ function createTaskElement() {
   const addNewTask = document.querySelector('#lista-tarefas');
   const textToTask = document.querySelector('#texto-tarefa');
   const li = document.createElement('li');
-  li.innerHTML = textToTask.value;
-  li.id = 'lista';
-  addNewTask.appendChild(li);
-  textToTask.value = '';
-  li.addEventListener('click', handleClickTaskElement);
-  li.addEventListener('dblclick', handleDblclickTaskElement);
+  if (textToTask.value !== '') {
+    li.innerHTML = textToTask.value;
+    li.id = 'lista';
+    addNewTask.appendChild(li);
+    textToTask.value = '';
+    li.addEventListener('click', handleClickTaskElement);
+    li.addEventListener('dblclick', handleDblclickTaskElement);
+  }
 }
 
 // Apaga todas as tarefas
@@ -59,3 +91,11 @@ buttonRemoveAllTasks.addEventListener('click', removeAllTasks);
 // Chama a função para apagar tarefas finalizadas
 const buttonRemoveCompletedTasks = document.querySelector('#remover-finalizados');
 buttonRemoveCompletedTasks.addEventListener('click', removeCompletedTasks);
+
+// Chama a função para salvar as tarefas no LocalStorage
+const buttonSaveTasks = document.querySelector('#salvar-tarefas');
+buttonSaveTasks.addEventListener('click', saveToLocalStorage);
+
+window.onload = function () {
+  getFromLocalStorage();
+};
