085769324ad3a08748c7088c3073c9acf14000c3
Dilenio Enderle <dilenio@gmail.com>
2020-07-29 12:50:46 -0300

    7 - Ao clicar em um item da lista, altere a cor de fundo do item para cinza rgb(128,128,128).

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 // javascript
-const buttonCreateTask = document.querySelector('#criar-tarefa');
-buttonCreateTask.addEventListener('click', function () {
+function handleTaskElement(event) {
+  const taskBackgroundColor = event.target;
+  taskBackgroundColor.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
+function createTaskElement() {
   const addNewTask = document.querySelector('#lista-tarefas');
   const textToTask = document.querySelector('#texto-tarefa');
   const li = document.createElement('li');
   li.innerHTML = textToTask.value;
+  li.id = 'lista';
   addNewTask.appendChild(li);
   textToTask.value = '';
-});
+  li.addEventListener('click', handleTaskElement);
+}
+
+const buttonCreateTask = document.querySelector('#criar-tarefa');
+buttonCreateTask.addEventListener('click', createTaskElement);
+
+// const li = document.getElementById('li');
+// if (document.getElementById('lista-tarefas').contains(li)) {
+//   const olTaskSelect = document.querySelector('#lista-tarefas');
+//   // const liTaskSelect = olTaskSelect.getElementsByTagName('li');
+//   olTaskSelect.addEventListener('click', function (event) {
+//     const changeColorTask = event.target;
+//     // rgb(128, 128, 128)
+//     changeColorTask.style.backgroundColor = 'red';
+//   });
+// }
