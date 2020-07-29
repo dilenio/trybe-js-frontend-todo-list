656ec5f56a7c9da12e478cfa8bb62a76fb02691d
Dilenio Enderle <dilenio@gmail.com>
2020-07-29 13:00:20 -0300

    8 - Não deve ser possível selecionar mais de um elemento da lista ao mesmo tempo.

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-// javascript
+// Lidar com o evento click na tarefa
+// Remove a seleção de itens e adiciona apenas naquele que foi clicado.
 function handleTaskElement(event) {
   const taskBackgroundColor = event.target;
+  const removeBackgroundColorTask = document.querySelectorAll('#lista');
+  for (let index = 0; index < removeBackgroundColorTask.length; index += 1) {
+    removeBackgroundColorTask[index].style.backgroundColor = '';
+  }
   taskBackgroundColor.style.backgroundColor = 'rgb(128, 128, 128)';
 }
 
+// Cria os elementos da tarefa
 function createTaskElement() {
   const addNewTask = document.querySelector('#lista-tarefas');
   const textToTask = document.querySelector('#texto-tarefa');
@@ -15,16 +21,6 @@ function createTaskElement() {
   li.addEventListener('click', handleTaskElement);
 }
 
+// Chama a função para criar os elementos da tarefa
 const buttonCreateTask = document.querySelector('#criar-tarefa');
 buttonCreateTask.addEventListener('click', createTaskElement);
-
-// const li = document.getElementById('li');
-// if (document.getElementById('lista-tarefas').contains(li)) {
-//   const olTaskSelect = document.querySelector('#lista-tarefas');
-//   // const liTaskSelect = olTaskSelect.getElementsByTagName('li');
-//   olTaskSelect.addEventListener('click', function (event) {
-//     const changeColorTask = event.target;
-//     // rgb(128, 128, 128)
-//     changeColorTask.style.backgroundColor = 'red';
-//   });
-// }
